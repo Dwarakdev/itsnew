7a1fbf70c2404ef60693bea94070a14bbf53056b
Dwarakdev <114311637+Dwarakdev@users.noreply.github.com>
2022-10-06 09:19:27 -0400

    Updated with New Changes

--- Demo.java
+++ Demo.java
@@ -1,5 +1,22 @@
-class Demo{
-   public static void main(String args[]){
-      System.out.println("Welcome to Tutorials Point");
-   }
+Pipeline {
+  agent {
+    node {
+      label 'JenkinsBuildNode'
+}
+}
+ stages {
+  stage('Code Checkout') {
+    steps {
+     checkout([
+        $class: 'GitSCM',
+        branches: [[name: 'develop']],
+        userRemoteConfigs: [[credentialsId: 'Arshu1997!',url: 'https://github.com/Dwarakdev/itsnew.git']]])
+    }
+  }
+  stage('shell command') {
+    steps {
+   sh "ls -al" 
+  }
+  }
+  }
 }
